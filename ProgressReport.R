3ee373b509b90aca0507794c6980651ed30a46c3
torischooler <61065061+torischooler@users.noreply.github.com>
2020-04-17 13:30:47 -0500

    Progress Report
    Progress Report that divides labor among Anna, Ashley, and Tori. So far, an .Rmd file has been made to begin downloading the correct python packages necessary for image analysis.

---
+++ ProgressReport.R
@@ -0,0 +1,26 @@
+#computing 2 Project Progress Report
+#April 17, 2020
+
+#Partners: Anna Harmon, Ashley Huebert, and Tori Schooler 
+
+#Goal: Use python to analyze images by measuring E.coli growth
+#taken from Anna Harmon's capstone project. Use t.test to to analyze growth
+#at different developmental stages. Use .plot function to graphically 
+#show results. 
+
+TO DO: 
+
+Tori 
+#importing python packages necessary to analyze images. Including 
+"opencv" "numpy" "scipy" "sckikit" "PIL" 
+#use "imutils" package in order to measure E.coli in images.
+#plot data using .plot with R
+
+Anna 
+#import images into computingprojectAAT.Rmd to be analyzed and gather measurement data. 
+
+Ashley 
+#perform t.test on size of E.coli in different developmental stages
+
+
+
